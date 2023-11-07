4bba509f6f3a40b6cd78fee6fe8d521c545cab92
Sebastian Chavez <sebastianchavez940@gmail.com>
2023-11-07 20:14:35 +0100

    printf almost ready

--- ft_printft.c
+++ ft_printft.c
@@ -51,10 +51,10 @@ void	ft_handle_p(unsigned long n, int *total, int i)
 		if (n < 16)
 		{
 			ft_handle_s("0x", total);
-			ft_put_chr(HEX[n % 16], total);
+			ft_put_chr(HEX_LOWER[n % 16], total);
 		}
 		ft_handle_p(n / 16, total, ++i);
-		ft_put_chr(HEX[n % 16], total);
+		ft_put_chr(HEX_LOWER[n % 16], total);
 	}
 
 }
@@ -90,8 +90,8 @@ void	ft_handle_u(unsigned int n, int *total)
 void	ft_handle_x(unsigned int n, int *total, int isLower)
 {
 	if (n >= 16)
-		ft_handle_x(n / 16, total, isLower)
-	if (isLower)
+		ft_handle_x(n / 16, total, isLower);
+	if (isLower == 1)
 		ft_put_chr(HEX_LOWER[n % 16], total);
 	else	
 		ft_put_chr(HEX_UPPER[n % 16], total);
@@ -105,14 +105,14 @@ void	all_possible(char *fmt,va_list args, int i, int *total)
 		ft_handle_s(va_arg(args, char *), total);
 	else if (fmt[i] == 'p')
 		ft_handle_p(va_arg(args, unsigned long int), total, 0);
-	else if (fmt[i] == 'd' || fmt[i] == 'i')
+	else if (fmt[i] == 'd' || fmt[i] == 'i')	
 		ft_handle_int(va_arg(args, int), total);
 	else if (fmt[i] == 'u')
 		ft_handle_u(va_arg(args, unsigned int), total);
 	else if (fmt[i] == 'x')
 		ft_handle_x(va_arg(args, unsigned int), total, 1);
 	else if (fmt[i] == 'X')
-		ft_handle_X(va_arg(args, unsigned int), total, 0);
+		ft_handle_x(va_arg(args, unsigned int), total, 0);
 	else if (fmt[i] == '%')
 		ft_put_chr('%', total);
 }
@@ -135,15 +135,5 @@ int	ft_printf(const char *format, ...) {
 	}
 	va_end(args);
 	return (total);
-}
-
-int main() {
-    int value = -42;
-    int *ptr = &value;
-
-	ft_printf("The address of the variable 'value' is: %u\n", value);
-    ft_printf("d = %d\n",value);
-
-    return 0;
 }
 
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%10s\n", "Hello");
-	return 0;
-}
